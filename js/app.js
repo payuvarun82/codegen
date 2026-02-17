ddc26ffa90443d4e94e05107f26db2f4b905bf4d
payuvarun82 <varun.maheshwari@payu.in>
2026-02-17 13:28:47 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1902,8 +1902,8 @@
                 udf1: 'Testing UDF 1',
                 udf2: 'Testing UDF2',
                 udf5: 'Sample_Invoice_11',
-                surl: 'https://payu.in/callback.php',
-                furl: 'https://payu.in/callback.php',
+                surl: 'https://payu.in/integrationlab/callback.php',
+                furl: 'https://payu.in/integrationlab/callback.php',
                 user_token: '1234567890'  // Default user_token for bank offers
             };
             
